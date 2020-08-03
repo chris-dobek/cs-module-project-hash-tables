d01ac30dc6f28189fe1b8568009268f7aac9e02a
Chris Dobek <dobek.christopher@gmail.com>
2020-08-03 15:34:34 -0400

    Day 1 MVP

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -21,7 +21,8 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
+        self.capacity = capacity
+        self.storage = [None] * capacity
 
 
     def get_num_slots(self):
@@ -62,7 +63,13 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        key_bytes = key.encode()
+        hash = 5381
+        for k_byte in key_bytes:
+            hash = hash * 33 + k_byte
+            hash &= 0xffffffff
+
+        return hash
 
 
     def hash_index(self, key):
@@ -81,7 +88,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        i = self.hash_index(key)
+        self.storage[i] = value
 
 
     def delete(self, key):
@@ -92,7 +100,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        i = self.hash_index(key)
+        self.storage[i] = None
 
 
     def get(self, key):
@@ -103,7 +112,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        i = self.hash_index(key)
+        return self.storage[i]
 
 
     def resize(self, new_capacity):
